30f5deacd427aeb13cb3f0e1741d90bb3e9f675d
Kent Fredric <kentfredric@gmail.com>
2012-02-04 16:44:13 +1300

    tidy code, refactor test

--- corpus/dist/perl-5-14/lib/DZ2.pm
+++ corpus/dist/perl-5-14/lib/DZ2.pm
@@ -3,14 +3,14 @@ use warnings;
 
 package DZ2 v5.5.7 {
 
-  # ABSTRACT: this is a sample package for testing Dist::Zilla;
+    # ABSTRACT: this is a sample package for testing Dist::Zilla;
 
-  sub main {
-    return 1;
+    sub main {
+        return 1;
+    }
+
+    1;
   }
-
-  1;
-}
 
 __END__
 
