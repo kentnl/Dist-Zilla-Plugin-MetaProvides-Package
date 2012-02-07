0402c3eec0ef11881171099291ecd6df1f609faa
Kent Fredric <kentfredric@gmail.com>
2012-02-07 22:03:01 +1300

    revert skip_underscore docs

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -23,7 +23,6 @@ In your C<dist.ini>:
     inherit_version = 0    ; optional
     inherit_missing = 0    ; optional
     meta_noindex    = 1    ; optional
-    skip_underscore = 1    ; optional
 
 =cut
 
@@ -93,29 +92,6 @@ eliminate it from the metadata shipped to L<Dist::Zilla>
 =cut
 
 has '+meta_noindex' => ( default => sub { 1 } );
-
-
-=head2 L<< C<skip_underscore>|Dist::Zilla::Role::MetaProvider::Provider/skip_underscore >>
-
-Filter out detected namespaces with a token with a leading C<_>, ie:
-
-    Foo::Bar::_internal::Baz;
-
-This is a convenience to provide sane defaults. For more controlled exclusion of namespaces, see L<Dist::Zilla::Plugin::MetaNoIndex>
-
-=over 4
-
-=item * skip_underscore = 0
-
-I<underscore>'d namespaces will be included.
-
-=item * DEFAULT: skip_underscore = 1
-
-I<underscore>'d namespaces will not be included.
-
-=back
-
-=cut
 
 =head1 ROLE SATISFYING METHODS
 
