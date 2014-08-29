7a35347e69069d77f7f88bf8debab2b3b6ba06d5
Kent Fredric <kentfredric@gmail.com>
2014-08-30 00:01:19 +1200

    Build results of 82a30b2 (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -5,7 +5,7 @@ use utf8;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.001001';
+our $VERSION = '2.001002';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
@@ -282,7 +282,7 @@ Dist::Zilla::Plugin::MetaProvides::Package - Extract namespaces/version from tra
 
 =head1 VERSION
 
-version 2.001001
+version 2.001002
 
 =head1 SYNOPSIS
 
