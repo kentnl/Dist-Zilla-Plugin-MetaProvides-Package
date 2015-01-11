592292ae9f383eaf6816e47e4839d0f26a30b22e
Kent Fredric <kentfredric@gmail.com>
2015-01-11 17:58:28 +1300

    Build results of 551180a (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.003000';
+our $VERSION = '2.003001';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
@@ -337,7 +337,7 @@ Dist::Zilla::Plugin::MetaProvides::Package - Extract namespaces/version from tra
 
 =head1 VERSION
 
-version 2.003000
+version 2.003001
 
 =head1 SYNOPSIS
 
