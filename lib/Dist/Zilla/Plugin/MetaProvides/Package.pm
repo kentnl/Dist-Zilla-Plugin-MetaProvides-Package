48b06c0d3459711f07a8dee935bcf00274368f87
Kent Fredric <kentfredric@gmail.com>
2015-01-11 17:57:07 +1300

    Build results of 2bd5f95 (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.002001';
+our $VERSION = '2.003000';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
@@ -337,7 +337,7 @@ Dist::Zilla::Plugin::MetaProvides::Package - Extract namespaces/version from tra
 
 =head1 VERSION
 
-version 2.002001
+version 2.003000
 
 =head1 SYNOPSIS
 
