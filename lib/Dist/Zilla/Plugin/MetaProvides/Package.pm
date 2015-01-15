0f5bace211e5363372c7984bef4bef3c253a1953
Kent Fredric <kentfredric@gmail.com>
2015-01-16 07:39:57 +1300

    v2.003001
     [Bugfix]
     - Avoid bug in Dzil <5.009 where Role::PPI doesn't load PPI::Document
    
     [Dependencies::Stats]
     - Dependencies changed since 2.003000, see misc/*.deps* for details
     - configure: (recommends: ↓1)
     - develop: +2 (recommends: ↓1, suggests: ↑1)
     - runtime: +1 (recommends: +1)
     - test: (recommends: ↓2)
    
     [Documentation]
     - Fix false documentation about defaults (dolmen++, #5)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.003001';
+our $VERSION = '2.003002';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
