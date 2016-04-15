c4ca15cc7edce49ca7952956c77abde48d116eec
Kent Fredric <kentfredric@gmail.com>
2016-04-15 18:53:12 +1200

    Build results of b2d6d8c (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.003002';
+our $VERSION = '2.003003';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
@@ -311,7 +311,7 @@ Dist::Zilla::Plugin::MetaProvides::Package - Extract namespaces/version from tra
 
 =head1 VERSION
 
-version 2.003002
+version 2.003003
 
 =head1 SYNOPSIS
 
