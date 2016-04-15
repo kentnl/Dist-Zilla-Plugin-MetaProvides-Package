ce3ea1938e8f7973b7525d081b082bc87134cf1e
Kent Fredric <kentfredric@gmail.com>
2016-04-15 18:51:46 +1200

    v2.003002
     - Remove QuickReference section as per #4
     - Enhance Synopsis documentation.
     - Remove unhelpful private documentation
    
     [Dependencies::Stats]
     - Dependencies changed since 2.003001, see misc/*.deps* for details
     - develop: +5 ↑3 -1 (suggests: ↑2)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.003002';
+our $VERSION = '2.003003';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
