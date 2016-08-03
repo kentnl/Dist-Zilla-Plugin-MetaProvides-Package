4effc023fabcc7807174e633ba2e988e934fa77f
Kent Fredric <kentfredric@gmail.com>
2016-08-04 05:05:41 +1200

    Build results of 5c226c1 (on master)

--- lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
+++ lib/Dist/Zilla/Plugin/MetaProvides/Package.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package Dist::Zilla::Plugin::MetaProvides::Package;
 
-our $VERSION = '2.004002';
+our $VERSION = '2.004003';
 
 # ABSTRACT: Extract namespaces/version from traditional packages for provides
 
@@ -327,7 +327,7 @@ Dist::Zilla::Plugin::MetaProvides::Package - Extract namespaces/version from tra
 
 =head1 VERSION
 
-version 2.004002
+version 2.004003
 
 =head1 SYNOPSIS
 
